d0f30b1ae2792068eb1746622d4b8893f70e2630
s-achurra <achurra7@gmail.com>
2017-08-10 14:27:07 -0700

    Add collapse nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const navLinks = $('.controls a, .name').click(scrollToElement);
 const overlays = $('.overlay');
 const modals = $('.modal');
+const menuNav = $('.toggle-nav');
+const controls = $('.controls');
 let closeButtons;
 let activeModal;
+
+// smooth scroll
 
 function scrollToElement (e) {
 	e.preventDefault();
@@ -10,6 +14,8 @@ function scrollToElement (e) {
 		scrollTop: $(e.target.hash).offset().top
 	}, 2000);
 };
+
+// modal
 
 function openModal () {
 	activeModal = $(`#${this.dataset.modal}`);
@@ -23,4 +29,19 @@ function closeModals() {
 	activeModal.css('opacity', '0');
 }
 
+// collapsed nav
+
+function handleMenu(e) {
+	e.preventDefault();
+	const offset = $('nav').position().top + 90;
+	controls.toggleClass('open-controls');
+	$('.open-controls').css('top', offset +'px');
+}
+
+function hideMenuControls() {
+	controls.removeClass('open-controls');
+}
+
 overlays.click(openModal);
+menuNav.click(handleMenu);
+controls.click(hideMenuControls);
